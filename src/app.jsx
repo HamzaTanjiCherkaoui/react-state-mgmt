d9eff7040f3adc2c702fce088a7d905aa384e3cd
pavanpodila <pavan.podila@gmail.com>
2017-03-25 23:22:07 -0400

    adding redux-observable

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ReduxSignupExample} from './redux-signup';
 import {ReduxSagaSignupExample} from './redux-saga-signup';
+import {Link} from 'react-router-dom';
 
 const examples = [
     {name: 'redux', title: 'Redux'},
@@ -16,34 +17,43 @@ export class App extends React.Component {
 
     render() {
         const {example} = this.state;
-        const activeClassName = 'active btn-primary';
 
         return (
             <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <div className="btn-group">
-                            {
-                                examples.map(x => {
 
-                                    return (
-                                        <button key={x.name}
-                                                type="button"
-                                                onClick={() => this.setState({example: x.name})}
-                                                className={`btn ${example === x.name ? activeClassName : 'btn-secondary'}`}>
-                                            {x.title}
-                                        </button>
-                                    );
-                                })
-                            }
-                        </div>
-                    </div>
-                </div>
-
+                <MenuBar selection={example}
+                         list={examples}
+                         onSelected={(name) => this.setState({example: name})} />
 
                 {example === 'redux' && <ReduxSignupExample />}
                 {example === 'redux-saga' && <ReduxSagaSignupExample />}
             </div>
         );
     }
+}
+
+function MenuBar({list, selection, onSelected}) {
+    const activeClassName = 'active btn-primary';
+
+    return (
+        <div className="row">
+            <div className="col">
+                <div className="btn-group">
+                    {
+                        list.map(x => {
+
+                            return (
+                                <button key={x.name}
+                                        type="button"
+                                        onClick={() => onSelected(x.name)}
+                                        className={`btn ${selection === x.name ? activeClassName : 'btn-secondary'}`}>
+                                    {x.title}
+                                </button>
+                            );
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/with-title.jsx
+++ src/components/with-title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function withTitle(title, Component) {
     return () => (
         <div>
-            <div className="clearfix">
+            <div className="clearfix mb-1">
                 <small className="badge badge-pill badge-primary float-right">{title}</small>
             </div>
 
